1dc24f2e56b80e2fc2e0392cd372ba1d0468b3ea
JoniP <jonipeltomaa@hotmail.com>
2025-09-30 13:18:18 +0300

    Tehtiin kategoria lista sivu valmiiksi

--- blogi/src/app/components/Footer.jsx
+++ blogi/src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <section className="lg:px-33 px-5 py-10 my-20 mt-30 border-t border-gray-800">
+      <section className="lg:px-33 px-5 py-10 my-20 mt-30 border-t border-gray-800 backdrop-blur-3xl">
         <div>
           <h1 className="text-3xl font-bold">Jonin Blogi</h1>
           <p className="text-gray-400 w-[70%]"></p>
