77badd4b4cb0f84e24fa98c8ef7250b894907985
JoniP <jonipeltomaa@hotmail.com>
2025-11-18 10:04:41 +0200

    blogi viestin kommentointi tehty

--- blogi/src/app/[slug]/page.js
+++ blogi/src/app/[slug]/page.js
@@ -8,6 +8,7 @@ import { formatDate } from '@/lib/utils';
 import { supabase } from '@/lib/supabaseClient';
 import { toast } from 'sonner';
 import { useParams, useRouter } from 'next/navigation';
+import { useAuth } from '@/context/AuthContext'
 
 
 export default function page() {
@@ -16,27 +17,32 @@ export default function page() {
     const params = useParams()
     const slug = params.slug
 
+    const { user, profile } = useAuth()
+
     const [article, setArticle] = useState([])
     const [comments, setComments] = useState([])
     const [likes, setLikes] = useState([])
     const [loading, setLoading] = useState(true)
 
+    const [newComment, setNewComment] = useState("")
+    const [submitting, setSubmitting] = useState(false)
+
     const fetchArticleData = async () => {
         setLoading(false)
 
-        const {data: articleData, error: articleError} = await supabase
-            .from("article")
-            .select(
-                `
-                    id, title, content, thumbnail, date_created, views, read_time, slug,
-                    category: category_id(title),
-                    author: profile_id(id, full_name, image, job_title),
-                    comment(id, comment, date_created, profile: profile_id(full_name, image)),
-                    like(id, profile_id, date_created)
-                `
-            )
-            .eq("slug", slug)
-            .single()
+    const {data: articleData, error: articleError} = await supabase
+        .from("article")
+        .select(
+            `
+                id, title, content, thumbnail, date_created, views, read_time, slug,
+                category: category_id(title),
+                author: profile_id(id, full_name, image, job_title),
+                comment(id, comment, date_created, profile: profile_id(full_name, image)),
+                like(id, profile_id, date_created)
+            `
+        )
+        .eq("slug", slug)
+        .single()
         
         if (articleError) {
             toast.error("Blogi viestien hakeminen epäonnistuis")
@@ -59,6 +65,43 @@ export default function page() {
         setComments(articleData?.comment)
         setLikes(articleData?.like)
         setLoading(false)
+    }
+
+    const handleAddComment = async () => {
+        if (!newComment.trim()) {
+            toast.error("Comment is required")
+            return
+        }
+
+        if (!user) {
+            toast.error("Kirjaudu sisään lisätäksesi kommentin")
+            return
+        }
+
+        setSubmitting(true)
+
+        const {data, error} = await supabase
+            .from("comment")
+            .insert([
+                {
+                    article_id: article?.id,
+                    profile_id: profile?.id,
+                    comment: newComment,
+                    date_created: new Date()
+                }
+            ])
+            .select("id, comment, date_created, profile:profile_id(full_name, image)")
+            .single()
+        
+        if (error) {
+            toast.error("Ei onnistunut lisämään kommenttia")
+            console.log("Comment insert error: ", error )
+        }else {
+            toast.success("Kommentti lisättiin onnistuneesti")
+            setComments((prev) => [...prev, data])
+            setNewComment("")
+        }
+        setSubmitting(false)
     }
 
     useEffect(() => {
@@ -107,14 +150,24 @@ export default function page() {
                             <div className='space-y-5 relative'>
                                 <div className='flex flex-col items-start gap-2'>
                                     <label htmlFor="">Koko Nimi</label>
-                                    <input className='border-3 border-[#e1d1ff7a] p-2 rounded-lg w-full' type="text" placeholder="Sinun nimesi" />
+                                    <input className='border-3 border-[#e1d1ff7a] p-2 rounded-lg w-full' type="text" value={profile?.full_name || "Testaaja"} readOnly placeholder="Sinun nimesi" />
                                 </div>
                                 <div className='flex flex-col items-start gap-2'>
                                     <label htmlFor="">Kommentit</label>
-                                    <textarea className='border-3 border-[#e1d1ff7a] p-2 rounded-lg w-full' type="text" placeholder="Kommentit" />
+                                    <textarea className='border-3 border-[#e1d1ff7a] p-2 rounded-lg w-full' type="text" value={newComment} onChange={(e) => setNewComment(e.target.value)} placeholder="Kommentit" />
                                 </div>
                                 <div>
-                                    <button className="lg:flex bg-gradient-to-r from-indigo-500 to-pink-500 cursor-pointer text-[15px] font-bold px-6 py-3 rounded-full border-0 me-3">Lähetä Kommentit <i className="fas fa-paper-plane ms-1"></i></button>
+                                    <button onClick={handleAddComment} className="lg:flex bg-gradient-to-r from-indigo-500 to-pink-500 cursor-pointer text-[15px] font-bold px-6 py-3 rounded-full border-0 me-3">
+                                        {submitting ? (
+                                            <>
+                                                Lähettää... <i className="fas fa-spinner fa-spin ms-1"></i>
+                                            </>
+                                        ) : (
+                                            <>
+                                                Lähetä Kommentit <i className="fas fa-paper-plane ms-1"></i>
+                                            </>
+                                        )}
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -124,7 +177,7 @@ export default function page() {
                                 {comments?.map((comment, index) => (
                                     <div className='bg-[#07050D] border border-[#110c1f] p-5 rounded-xl' key={comment?.id}>
                                         <div className='flex items-center gap-3'>
-                                            <Image width={100} height={100} src={comment?.profile.image} alt='' className='w-[2rem] h-[2rem] rounded-full' />
+                                            <Image width={100} height={100} src={comment?.profile.image || defaultAvatar} alt='' className='w-[2rem] h-[2rem] rounded-full' />
                                             <div>
                                                 <h1 className='text-lg font-bold'>{comment?.profile.full_name}</h1>
                                                 <p className='text-xs'>{formatDate(comment?.date_created)}</p>
